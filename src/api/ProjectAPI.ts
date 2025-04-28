479f6918fb4d09df8780d1ddf1e88146496d48f3
Melvin Fernando Ocotan Morales <melvin.ocotan@catolica.edu.sv>
2025-04-27 23:55:27 -0600

    delete project

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -41,3 +41,44 @@ export async function getProjectById(id: Project['_id']) {
     }
 }
 
+export async function getFullProject(id: Project['_id']) {
+    try {
+        const { data } = await api(`/projects/${id}`)
+        const response = projectSchema.safeParse(data)
+        if(response.success) {
+            return response.data
+        }
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+type ProjectAPIType = {
+    formData: ProjectFormData
+    projectId: Project['_id']
+}
+
+export async function updateProject({formData, projectId} : ProjectAPIType ) {
+    try {
+        const { data } = await api.put<string>(`/projects/${projectId}`, formData)
+        return data
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function deleteProject(id: Project['_id']) {
+    try {
+        const url = `/projects/${id}`
+        const { data } = await api.delete<string>(url)
+        return data
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
