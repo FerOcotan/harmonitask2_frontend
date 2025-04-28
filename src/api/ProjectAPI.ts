2f13004ed70742a56d961fb4878b1f3320c0f4ab
Melvin Fernando Ocotan Morales <melvin.ocotan@catolica.edu.sv>
2025-04-27 23:27:06 -0600

    agregando form y funcionalidad de editar

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,11 +1,39 @@
 import api from "@/lib/axios";
-import { ProjectFormData,} from "../types";
+import { Project, ProjectFormData, dashboardProjectSchema, editProjectSchema, projectSchema } from "../types";
 import { isAxiosError } from "axios";
 
 export async function createProject(formData: ProjectFormData) {
     try {
         const { data } = await api.post('/projects', formData)
         return data
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function getProjects() {
+    try {
+        const { data } = await api('/projects')
+        const response = dashboardProjectSchema.safeParse(data)
+        if(response.success) {
+            return response.data
+        }
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function getProjectById(id: Project['_id']) {
+    try {
+        const { data } = await api(`/projects/${id}`)
+        const response = editProjectSchema.safeParse(data)
+        if(response.success) {
+            return response.data
+        }
     } catch (error) {
         if(isAxiosError(error) && error.response) {
             throw new Error(error.response.data.error)
