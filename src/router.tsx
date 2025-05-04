01eec1c735e2dbe02b514abd6502a85d180dd086
Melvin Fernando Ocotan Morales <melvin.ocotan@catolica.edu.sv>
2025-05-04 17:24:21 -0600

    confirmation and register form and functionality

--- src/router.tsx
+++ src/router.tsx
@@ -5,6 +5,11 @@ import CreateProjectView from "./views/projects/CreateProjectView";
 
 import EditProjectView from "./views/projects/EditProjectView";
 import ProjectDetailsView from "./views/projects/ProjectDetailsView";
+import AuthLayout from "./layouts/AuthLayout";
+import LoginView from "./views/auth/LoginView";
+import RegisterView from "./views/auth/RegisterView";
+import ConfirmAccountView from "./views/auth/ConfirmAccountView";
+import RequestNewCodeView from "./views/auth/RequestNewCodeView";
 
 export default function Router() {
     return (
@@ -17,6 +22,16 @@ export default function Router() {
                     <Route path="/projects/:projectId" element={<ProjectDetailsView/>} index />
                     <Route path="/projects/:projectId/edit" element={<EditProjectView/>} index />
                 </Route>
+
+
+                <Route element={<AuthLayout />}>
+                    <Route path="/auth/login" element={<LoginView/>} index />
+                    <Route path="/auth/register" element={<RegisterView/>} index />
+                    <Route path="/auth/confirm-account" element={<ConfirmAccountView/>} index />
+                    <Route path="/auth/request-code" element={<RequestNewCodeView/>} index />
+                
+                </Route>
+
             </Routes>
         </BrowserRouter>
     );
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,35 @@
 import { z } from 'zod'
 
+/** Auth & Users */
+const authSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    current_password: z.string(),
+    password: z.string(),
+    password_confirmation: z.string(),
+    token: z.string()
+})
+
+type Auth = z.infer<typeof authSchema>
+export type UserLoginForm = Pick<Auth, 'email' | 'password'>
+export type UserRegistrationForm = Pick<Auth, 'name' | 'email' | 'password' | 'password_confirmation'>
+export type RequestConfirmationCodeForm = Pick<Auth, 'email'>
+export type ForgotPasswordForm = Pick<Auth, 'email'>
+export type NewPasswordForm = Pick<Auth, 'password' | 'password_confirmation'>
+export type UpdateCurrentUserPasswordForm = Pick<Auth, 'current_password' | 'password' | 'password_confirmation'>
+export type ConfirmToken = Pick<Auth, 'token'>
+export type CheckPasswordForm = Pick<Auth, 'password'>
+
+/** Users */
+export const userSchema = authSchema.pick({
+    name: true,
+    email: true
+}).extend({
+    _id: z.string()
+})
+export type User = z.infer<typeof userSchema>
+export type UserProfileForm = Pick<User, 'name' | 'email'>
+
 /** Tasks */
 export const taskStatusSchema = z.enum(["pending", "onHold", "inProgress", "underReview", "completed" ])
 export type TaskStatus = z.infer<typeof taskStatusSchema>
